c36d694803fe9a090b84179b05b30d6a4dd79ca4
Hemant Kumar <hekumar@redhat.com>
2017-10-31 14:34:29 -0400

    Add some code

--- print_attached_pv.py
+++ print_attached_pv.py
@@ -7,8 +7,16 @@ import time
 import re
 import sys
 
+class VolumeInfo(object):
+    def __init__(self, pv_name, ebs_id, instance_id, device_name):
+        self.pv_name = pv_name
+        self.ebs_id = ebs_id
+        self.instance_id = instance_id
+        self.device_name = device_name
+
 class AttachedVolume(object):
-    def __init__(self):
+    def __init__(self, profile):
+        self.profile = profile
         self.pvc_regex = re.compile("^pvc")
 
     def run(self):
@@ -22,17 +30,35 @@ class AttachedVolume(object):
 
     def check_volume(self, ebs_id, pv_name):
         try:
-            output = subprocess.check_output(["aws", "ec2", "describe-volumes", "--volume-ids", ebs_id, "--profile", "v3-paid-prod"])
+            output = subprocess.check_output(["aws", "ec2", "describe-volumes", "--volume-ids", ebs_id, "--profile", self.profile])
             ebs_data = json.loads(output)
             volume = ebs_data["Volumes"][0]
             state = volume["State"]
             if state == "in-use":
-                print "pv %s with volume %s is in use" % (pv_name, ebs_id)
+                attachment = volume["Attachments"][0]
+                instance_id = attachment["InstanceId"]
+                device_name = attachment["Device"]
+                volume_info = VolumeInfo(pv_name, ebs_id, instance_id, device_name)
+                self.check_for_mount(volume_info)
         except Exception, e:
             print "Volume not found"
+
+    def check_for_mount(self, volume_info):
+        try:
+            # running command
+            # ossh -o "StrictHostKeyChecking=no" -c "grep \/dev\/xvdbz /proc/mounts" i-foobar
+            grep_command = "grep %s /proc/mounts" % (volume_info.ebs_id)
+            output = subprocess.check_output(["ossh", "-o", "StrictHostKeyChecking=no", "-c", grep_command, volume_info.instance_id])
+            if self.check_for_mount_point(output, volume_info):
+                print "%s : %s : %s" % (volume_info.ebs_id, volume_info.instance_id, volume_info.device_name)
+        except Exception, e:
+            print "Error checking mount status for %s and host %s" % (volume_info.ebs_id, volume_info.instance_id)
+
+    def check_for_mount_point(self, output, volume_info):
+        return true
 
 
 
 if __name__ == "__main__":
-    a = AttachedVolume()
+    a = AttachedVolume(sys.argv[1])
     a.run()
