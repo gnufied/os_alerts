a9b96d9cc1f6497db1999df0ef2913ea41316806
Hemant Kumar <hekumar@redhat.com>
2017-11-09 14:24:53 -0500

    Add support for finding attaching volumes

--- dirty_node.rb
+++ dirty_node.rb
@@ -1,9 +1,12 @@
 require 'json'
+require "time"
 
 class DirtyNode
-  def initialize(file_path, profile)
+  def initialize(op_name, file_path, profile, region)
+    @op_name = op_name.strip()
     @file_path = file_path
     @profile = profile
+    @region = region
   end
 
   def print_dirty_nodes()
@@ -20,12 +23,16 @@ class DirtyNode
     end
 
     @dirty_nodes.each do |node, val|
-      reboot_node(node)
+      if @op_name == "reboot"
+        reboot_node(node)
+      else
+        puts "Volume stuck on node #{node}"
+      end
     end
   end
 
   def reboot_node(node)
-    command = "aws ec2 reboot-instances --instance-ids #{node} --profile=#{@profile}"
+    command = "aws ec2 reboot-instances --instance-ids #{node} --profile=#{@profile} --region=#{@region}"
     puts "Running - #{command}"
     system(command)
   end
@@ -35,13 +42,27 @@ class DirtyNode
 
     if !attachments.empty?
       attachment = attachments[0]
-      attachment["State"] == "attaching"
+      attachment_state = attachment["State"]
+      if attachment_state == "attaching"
+        attach_time_str = attachment["AttachTime"]
+        stuck_in_attaching?(attach_time_str)
+      else
+        false
+      end
     else
       false
     end
   end
+
+  def stuck_in_attaching?(attach_time_str)
+    attach_time = Time.parse(attach_time_str)
+    time_diff = (Time.now() - attach_time)/3600
+    # if time difference is greater than 2 hours
+    time_diff > 2
+  end
 end
 
 
-a = DirtyNode.new(ARGV[0], ARGV[1])
+
+a = DirtyNode.new(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
 a.print_dirty_nodes
